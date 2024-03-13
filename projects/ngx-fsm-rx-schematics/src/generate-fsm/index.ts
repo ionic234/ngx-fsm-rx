ee4bbbb87fee65bd817b8a868b1186f69605c5eb
chrisg <ionic234@gmail.com>
2024-03-13 16:35:51 +1100

    first schematic working

--- projects/ngx-fsm-rx-schematics/src/generate-fsm/index.ts
+++ projects/ngx-fsm-rx-schematics/src/generate-fsm/index.ts
@@ -1,11 +1,13 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import { Schema } from './schema';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function generateFsm(_options: any): Rule {
+export function generateFsm(options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    console.log("hello");
+    const name = options.name;
+    console.log("hello", name);
+    tree.create("hello.js", `console.log(hi ${name}")`);
     return tree;
   };
 }
